ab22c2fd55df671d7dbbfb7927aa1ec1168d5828
Fenrir889 <danielwatermaster@hotmail.com>
2016-10-21 23:34:35 -0500

    Spawner y BulletDamage

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	public int _damage;
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.transform.tag == "Enemy") 
+		{
+			hit.SendMessage ("TakeDamage", (float)_damage);
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour {
+	public float Speed, Health;
+	public Image _health;
+	// Use this for initialization
+	void Start () {
+		Health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (-Speed * Time.deltaTime, 0, 0);
+	}
+
+	public void TakeDamage(float Dmg)
+	{
+		Health -= Dmg;
+		_health.fillAmount -= (float)(Dmg/100);
+		if (Health <= 0) 
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
